6a1a7fa09ec64dfab89bd61879b3e54ca72c7f31
igoorcodi <igoor.codi@gmail.com>
2022-11-04 13:47:32 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+let delay = -0.4;
+
+function createCard(date, day, games) {
+    delay = delay + 0.4;
+    return `
+        <div class="card" style="animation-delay: ${delay}s">
+            <h2>${date}<span>${day}</span></h2>
+            <ul>
+                ${games}
+            </ul>
+        </div>
+    `
+}
+
+function createGame(player1, hour, player2) {
+    return `
+        <li>
+            <img src="./assets/SVGs/icon-${player1}.svg" alt="Bandeira do ${player1}">
+            <strong>${hour}</strong>
+            <img src="./assets/SVGs/icon-${player2}.svg" alt="Bandeira da ${player2}">
+        </li>
+    `
+}
+
+document.querySelector("#cards").innerHTML =
+    createCard("24/11", "quinta", createGame("portugal", "13:00", "ghana") + createGame("brazil", "16:00", "serbia")) +
+    createCard("28/11", "segunda", createGame("switzerland", "13:00", "brazil") + createGame("portugal", "16:00", "uruguai"))
